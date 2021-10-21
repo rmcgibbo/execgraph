0fe3f1f165608a6a5240543e9496ac2d3e4fc395
Robert T. McGibbon <rmcgibbo@gmail.com>
2021-10-21 10:53:26 -0400

    Update cargo hash for nix

--- default.nix
+++ default.nix
@@ -34,7 +34,7 @@ in buildPythonPackage rec {
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    sha256 = "sha256-qk7oqjXl1+ED2MmZJ7j6GVIDapatyuasmRVByYy7Qrk=";
+    sha256 = "sha256-LUAj81o8x+kTKyrnj70f+25LqM8KkV9LABdhP1cnzQI=";
     # sha256 = "0000000000000000000000000000000000000000000000000000";
   };
 
