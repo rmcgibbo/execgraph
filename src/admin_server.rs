4859fcc57cf917ae276917780d36fc558c2d33a8
Robert T. McGibbon <rmcgibbo@gmail.com>
2023-05-07 13:58:43 -0400

    Be a little bit more hygenic about the admin socket

--- src/admin_server.rs
+++ src/admin_server.rs
@@ -83,17 +83,55 @@ pub async fn run_admin_service_forever(state: Arc<State<'static>>, token: Cancel
             panic!("Error: {}", error);
         }
     }
-
     let service = admin_router(state).into_make_service();
     use tokio_stream::wrappers::UnixListenerStream;
 
+    // If path alreay exists, then a previous program may have used this path (because it has the same PID)
+    // but shutdown uncleanly and never removed the file (kill -9 or something). Since the file is in /run, it's
+    // specific to this box. And since we have our current PID, the other instance that made this file must
+    // be over (this wouldn't be true if we were making this file on a shared filesystem).
+    let _ = std::fs::remove_file(&path);
     let uds = tokio::net::UnixListener::bind(path.clone()).unwrap();
     let stream = UnixListenerStream::new(uds);
     let acceptor = hyper::server::accept::from_stream(stream);
     let server = Server::builder(acceptor).serve(service);
+    tokio::spawn(clean_up_socket_dir(path.parent().unwrap().to_path_buf()));
     let graceful = server.with_graceful_shutdown(token.hard_cancelled());
     if let Err(err) = graceful.await {
         error!("Server error: {}", err);
     }
     std::fs::remove_file(path).unwrap();
+}
+
+async fn clean_up_socket_dir(socket_dir: PathBuf) -> std::io::Result<()> {
+    // No rush on this, but go through any socket files in the directory and if the process doesn't
+    // exist anmore, just remove then. Since we can't clean up our own socket file ourselves with
+    // 100% accuracy, let later instances of the program do some removal.
+    use regex::Regex;
+    use std::os::unix::fs::FileTypeExt;
+    use sysinfo::{Pid, SystemExt};
+    let socket_re = Regex::new(&format!(r"{}-(\d+).sock", ADMIN_SOCKET_PREFIX)).unwrap();
+    let system = sysinfo::System::new();
+
+    for entry in std::fs::read_dir(socket_dir)? {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            let pathstr = path.file_name().unwrap().to_string_lossy();
+            let captures = socket_re.captures(&pathstr);
+            if entry.file_type()?.is_socket() && captures.is_some() {
+                let captures = captures.unwrap();
+                let pid = captures
+                    .get(1)
+                    .map_or("", |m| m.as_str())
+                    .parse::<usize>()
+                    .unwrap();
+                if system.process(Pid::from(pid)).is_none() {
+                    tracing::debug!("Removing unused socket file {:?}", path);
+                    std::fs::remove_file(path).unwrap();
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
